d6da0897d93bfec64f1d49132aa939326274e02c
Talits <talita.bernardesp@gmail.com>
2023-02-20 19:39:51 -0300

    [main]: first script

---
+++ rabbit-send.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import pika
+credentials = pika.PlainCredentials('guest', 'guest')
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost',
+                                       55001))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello2')
+
+channel.basic_publish(exchange='', routing_key='hello', body='Hello Ada!')
+print(" [x] Mensagem Enviada'")
+connection.close()
