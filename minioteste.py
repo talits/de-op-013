2c946688582b9eef374916de63bd61e2dad37e69
Talita Bernardes Pereira <talita.bernardesp@gmail.com>
2023-03-19 15:36:31 -0300

    Update minioteste.py

--- minioteste.py
+++ minioteste.py
@@ -9,9 +9,10 @@ if __name__ == "__main__":
     MINIO_API_HOST = "http://localhost:9000"
     MINIO_CLIENT = Minio("localhost:9000", access_key=ACCESS_KEY, secret_key=SECRET_KEY, secure=False)
     found = MINIO_CLIENT.bucket_exists("minioteste")
- 
-    MINIO_CLIENT.make_bucket("minioteste")
-    print("It is successfully uploaded to bucket")
+    if not found:
+       MINIO_CLIENT.make_bucket("minioteste")
+    else:
+       print("Bucket already exists")
 
     MINIO_CLIENT.fput_object("minioteste", "gato.jpeg", LOCAL_FILE_PATH,)
     print("It is successfully uploaded to bucket")
